3b3ba6759532315ad338f38a70a357edde682c5c
rukevwe ubioworo <159934989+Indeedelight12345@users.noreply.github.com>
2025-07-27 13:57:23 +0000

    update the terrafrom script

--- terraform/output.tf
+++ terraform/output.tf
@@ -33,3 +33,7 @@ output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
   value       = aws_instance.web.public_ip
 }
+output "ec2_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
