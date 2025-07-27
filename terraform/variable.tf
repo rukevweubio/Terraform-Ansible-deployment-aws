73d826a56267df16fca171b1390c826f750b897a
rukevwe ubioworo <159934989+Indeedelight12345@users.noreply.github.com>
2025-07-27 13:26:34 +0000

    update the terraformscript

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -18,14 +18,6 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  type        = string
+  default       = "ami-0c55b159cbfafe1f0" 
 }
 
-variable "vpc_id" {
-  description = "VPC ID where the EC2 instance will be launched"
-  type        = string
-}
-variable "subnet_id" {
-  description = "Subnet ID where the EC2 instance will be launched"
-  type        = string
-}
