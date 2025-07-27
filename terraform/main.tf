2fe1d6bfa0fa750c97c07d596efdfe3cbcf05acd
rukevwe ubioworo <159934989+Indeedelight12345@users.noreply.github.com>
2025-07-27 12:39:25 +0000

    update the terraformscript

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,96 @@
-re
+provider "aws" {
+  region = "us-east-1"
+}
+
+#vpc
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+#Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+# Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "subnet_association" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# Security Group
+resource "aws_security_group" "my_web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.main_subnet.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              echo "Hello from Terraform + EC2 + VPC" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
